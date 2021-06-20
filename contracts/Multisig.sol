d11d6321dadd9fd14ced3e8a224d5785196b51e6
ibk <8611762+esengulov@users.noreply.github.com>
2021-06-21 01:00:25 +0600

    refactor multisig to allow interaction with team

--- contracts/Multisig.sol
+++ contracts/Multisig.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract Multisig {
+import "./Team.sol";
 
-  mapping(address => bool) public isOwner;
+contract Multisig is Team {
+
   uint approvalLimit;
 
   struct TransferRequest {
@@ -14,44 +15,31 @@ contract Multisig {
     bool completed;
   }
 
-  TransferRequest[] public transferRequests;
-  mapping(address => mapping(uint => bool)) public voted;
+  TransferRequest[] transferRequests;
+  mapping(address => mapping(uint => bool)) voted;
 
-  modifier ownersOnly {
-    require(isOwner[msg.sender] == true, "Not accessible to unauthorized owners");
-    _;
-  }
-
-  constructor(address _owner1, address _owner2, address _owner3, uint _limit) public {
-    isOwner[_owner1] = true;
-    isOwner[_owner2] = true;
-    isOwner[_owner3] = true;
+  constructor(uint _limit, address[] memory members) Team(members) public {
     approvalLimit = _limit;
   }
 
   // accept public deposits
   function deposit() public payable {
-    require(msg.value >= 1000);
   }
 
-  function transferRequest(uint _amount, address payable _recipient) public ownersOnly {
+  function transferRequest(uint _amount, address payable _recipient) public teamOnly {
     require(address(this).balance >= _amount, "Requested Amount exceeds available balance");
-    transferRequests.push(TransferRequest(_amount, _recipient, approvalLimit, transferRequests.length, false));
+    transferRequests.push(TransferRequest(_amount, _recipient, 0, transferRequests.length, false));
     approveRequest(transferRequests.length);
   }
 
-  function approveRequest(uint _id) public {
+  function approveRequest(uint _id) public teamOnly {
     require(voted[msg.sender][_id] == false, "user already voted on this proposal");
     voted[msg.sender][_id] = true;
     transferRequests[_id].approvalCount ++;
-
     uint _approvals = transferRequests[_id].approvalCount;
-
     if(_approvals >= approvalLimit) {
-      uint _prevBalance = address(this).balance;
       transferRequests[_id].recipient.transfer(transferRequests[_id].amount);
-      assert(address(this).balance == _prevBalance - transferRequests[_id].amount);
       transferRequests[_id].completed = true;
-      }
     }
+  }
 }
